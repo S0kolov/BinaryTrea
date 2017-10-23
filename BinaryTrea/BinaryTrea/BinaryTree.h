283e484bb9f3992fa99b03c47e6655d1dc5658cd
Alexander Sokolovskiy <sasha.sokolovskiy99@gmail.com>
2017-10-23 16:16:52 +0300

    AddItem_2

--- BinaryTrea/BinaryTrea/BinaryTree.h
+++ BinaryTrea/BinaryTrea/BinaryTree.h
@@ -10,54 +10,84 @@ class BinaryTree
 	T _item;
 	BinaryTree* left;
 	BinaryTree* right;
+
+	bool AddRight(int key, T newItem);
+	bool AddLeft(int key, T newItem);
+
 public:
 
-	bool AddNewItem(int key,T newItem) {
-		BinaryTree* temp;
-		//if (newItem._key >= this->_key)	GoRight();
-		//else GoLeft();
-		tepm = (newItem._key >= this->_key) ? GoRight() : GoLeft();
-		if (temp == this)
-			if (newItem._key >= this->_key)
-				return AddRight(key, newItem);
-			else
-				return AddLeft(key, newItem);
-	}
-	bool AddRight(int key,T newItem ) {
-		return( (this->right = new BinaryTree(key, newItem))!=nullptr)? true:false;
-	}
+	static bool AddNewItem(int key, T newItem) ;
 
-	bool AddLeft(int ket, T newItem) {
-		return((this->left = new BinaryTree(key, newItem)) != nullptr) ? true : false;
-	}
+	static BinaryTree<T>* GoRight(BinaryTree<T>* temp) ;
+	static BinaryTree<T>* GoLeft(BinaryTree<T>* temp) ;
 
-	BinaryTree* GoRight() {
-		if (this->right != nullptr)
-			return this->right;
-		else
-			return this;
-	}
-	BinaryTree* GoLeft() {
-		if (this->left != nullptr)
-			return this->left;
-		else
-			return this;
-	}
+	bool AddNewItem(int key, BinaryTree newItem);
 
-	bool AddNewItem(int key,BinaryTree newItem) {
-		if (item.Left == NULL && item.Right == NULL) {
-			return AddNewItem(key,item._item);
-		}
-		return false;
-	}
-
-	BinaryTree(int key,T newItem=NULL) {
-		_key = key;
-		_item = item;
-		left = NULL;
-		right = NULL;
-	}
+	BinaryTree(int key, T newItem = NULL);
 	~BinaryTree() {}
 
 };
 
+template<typename T>
+bool BinaryTree<T>::AddNewItem(int key, T newItem)
+{
+	BinaryTree<T>* temp = nullptr;
+	//if (newItem._key >= this->_key)	GoRight();
+	//else GoLeft();
+	temp = (key >= this->_key) ? GoRight(this) : GoLeft(this);
+	if (temp == this)
+		if (key >= this->_key)
+			return BinaryTree::AddRight(key, newItem);
+		else
+			return BinaryTree::AddLeft(key, newItem);
+	else
+		return this->AddNewItem(key, newItem);
+}
+
+template<typename T>
+inline bool BinaryTree<T>::AddRight(int key, T newItem)
+{
+	return((this->right = new BinaryTree<T>(key, newItem)) != nullptr) ? true : false;
+}
+
+template<typename T>
+inline bool BinaryTree<T>::AddLeft(int key, T newItem)
+{
+	return((this->left = new BinaryTree<T>(key, newItem)) != nullptr) ? true : false;
+}
+
+template<typename T>
+BinaryTree<T>* BinaryTree<T>::GoRight(BinaryTree<T>* temp)
+{
+	if (temp->right != nullptr)
+		return temp->right;
+	else
+		return temp;
+}
+
+template<typename T>
+BinaryTree<T>* BinaryTree<T>::GoLeft(BinaryTree<T>* temp)
+{
+	if (temp->left != nullptr)
+		return temp->left;
+	else
+		return temp;
+}
+
+template<typename T>
+bool BinaryTree<T>::AddNewItem(int key, BinaryTree newItem)
+{
+	if (item.Left == NULL && item.Right == NULL) {
+		return AddNewItem(key, item._item);
+	}
+	return false;
+}
+
+template<typename T>
+BinaryTree<T>::BinaryTree(int key, T newItem)
+{
+	this->_key = key;
+	this->_item = newItem;
+	this->left = NULL;
+	this->right = NULL;
+}
